f47471cd0f9da9de935acfd8195aa7be17b54d19
IU-Studies <notes.in.drive.forever@gmail.com>
2025-01-21 18:29:17 +0530

    Create Basic Programs Program to Find the Square Root.py

---
+++ APP - Python Classwork Examples/Basic Programs Program to Find the Square Root.py
@@ -0,0 +1,10 @@
+#Basic Programs Program to Find the Square Root
+
+def sq_root(num):
+    return num ** (1/2)
+
+user_input = int(input("Enter a number:- "))
+
+calculate_sqrt = sq_root(user_input)
+
+print(calculate_sqrt)
