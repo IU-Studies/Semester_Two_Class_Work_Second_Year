93d95ba7a68db0ad2dbd2c1b6fd85311816a21b6
IU-Studies <notes.in.drive.forever@gmail.com>
2025-01-21 18:25:39 +0530

    Create Program to Find the Factorial of a Number.py

---
+++ APP - Python Classwork Examples/Program to Find the Factorial of a Number.py
@@ -0,0 +1,13 @@
+#Program to Find the Factorial of a Number
+
+def factorial(num):
+    fact = 1
+    for i in range(1, num+1):
+        fact *= i
+    return fact
+
+user_input = int(input("Enter a number:- "))
+
+calculate_factorial = factorial(user_input)
+
+print(calculate_factorial)
