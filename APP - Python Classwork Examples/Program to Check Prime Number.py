57ae0422c05b8d3ac91ad6f92e9b92acb33f6a31
IU-Studies <notes.in.drive.forever@gmail.com>
2025-01-23 19:23:28 +0530

    Update Program to Check Prime Number.py

--- APP - Python Classwork Examples/Program to Check Prime Number.py
+++ APP - Python Classwork Examples/Program to Check Prime Number.py
@@ -1,15 +1,12 @@
 #Program to Check Prime Number
 
 def check_prime(number):
-    num_range = number // 2
-    count = 0
-    for i in range(2, num_range):
+    if number <= 1:
+            return f"{number} is not a Prime Number"
+    for i in range(2, number):
         if number % i == 0:
-            count += 1
-    if count > 0:
-        return f"{number} is not a Prime Number"
-    else:
-        return f"{number} is a Prime Number"
+            return f"{number} is not a Prime Number"
+    return f"{number} is a Prime Number"
 
 user_input = int(input("Enter a number:- "))
 
