526c78ec8aee6d02bf51e02474345205d6d28db6
IU-Studies <notes.in.drive.forever@gmail.com>
2025-01-24 18:01:10 +0530

    Create Program to Find Factorial of Number Using Recursion.py

---
+++ APP - Python Classwork Examples/Program to Find Factorial of Number Using Recursion.py
@@ -0,0 +1,14 @@
+#Program to Find Factorial of Number Using Recursion
+
+def factorial(n):
+    fact = 1
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+user_input = int(input("Enter a number:- "))
+
+result = factorial(user_input)
+
+print(result)
