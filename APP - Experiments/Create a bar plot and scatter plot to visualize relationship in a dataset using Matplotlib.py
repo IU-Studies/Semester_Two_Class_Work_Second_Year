cb398939399c35e28efedf6c910f62d7d1978eca
IU Studies <notes.in.drive.forever@gmail.com>
2025-02-17 09:03:05 +0530

    Create Create a bar plot and scatter plot to visualize relationship in a dataset using Matplotlib.py

---
+++ APP - Experiments/Create a bar plot and scatter plot to visualize relationship in a dataset using Matplotlib.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv(r"C:\Users\IU\Downloads\iris.csv")
+
+plt.figure(figsize=(8, 5))
+sns.countplot(data=df, x='species',hue='species', palette='Set2')
+plt.title('Count Iris Species')
+plt.show()
+
+plt.figure(figsize=(8, 5))
+sns.scatterplot(data=df, x='sepal_length', y='sepal_width', hue='species', palette='Set2')
+plt.title('Sepal Length and Sepal Width')
+plt.show()
