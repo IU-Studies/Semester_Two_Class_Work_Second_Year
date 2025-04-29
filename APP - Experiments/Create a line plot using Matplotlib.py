2e6f0ca217a23f36077055e0e72e1cbd22eaa7ce
IU Studies <notes.in.drive.forever@gmail.com>
2025-04-29 07:26:01 +0530

    Update Create a line plot using Matplotlib.py

--- APP - Experiments/Create a line plot using Matplotlib.py
+++ APP - Experiments/Create a line plot using Matplotlib.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv(r"C:\Users\iu\Downloads\temp.csv")
 
-df['dt'] = pd.to_datetime(df['dt'])
+df['Year'] = pd.to_numeric(df['Year'], errors='coerce')
 
 plt.figure(figsize=(10,6))
-plt.plot(df['dt'], df['AverageTemperature'], label='Temperature (°C)', color='tab:blue')
-plt.title('Temperature Over Time (Abidjan, Côte D\'Ivoire)', fontsize=14)
-plt.xlabel('Date', fontsize=12)
-plt.ylabel('Temperature (°C)', fontsize=12)
+plt.plot(df['Year'], df['Average_Fahrenheit_Temperature'], label='Temperature (°F)', color='tab:blue')
+plt.title('Average Temperature Over Years', fontsize=14)
+plt.xlabel('Year', fontsize=12)
+plt.ylabel('Temperature (°F)', fontsize=12)
 plt.xticks(rotation=45)
 plt.legend()
 plt.tight_layout()
