f82af3595e13cf0fe83245122be54678aa95bf86
IU Studies <notes.in.drive.forever@gmail.com>
2025-04-29 07:02:24 +0530

    Update and rename Build a Linear Regression model using Scikit-learn to predict student scores based on study hours and evaluate its performance using mean squared error.py to Build a Linear Regression model.py

--- APP - Experiments/Build a Linear Regression model.py
+++ APP - Experiments/Build a Linear Regression model.py
@@ -1,10 +1,13 @@
+# Build a Linear Regression model using Scikit-learn to predict student scores based
+# on study hours and evaluate its performance using mean squared error.
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv(r"C:\Users\mayur\Downloads\score.csv")
+df = pd.read_csv(r"C:\Users\iu\Downloads\score.csv")
 X = df[["Hours"]]
 y = df["Scores"]
 
